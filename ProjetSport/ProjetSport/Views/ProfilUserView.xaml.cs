f153de1d018b0e491ae799a7e23ab1a576e1300b
Arthur Dumortier <arthur.dumortier@saint-benigne.fr>
2023-04-20 15:00:14 +0200

    new

--- ProjetSport/ProjetSport/Views/ProfilUserView.xaml.cs
+++ ProjetSport/ProjetSport/Views/ProfilUserView.xaml.cs
@@ -7,13 +7,15 @@ public partial class ProfilUserView : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
 		CollectionView.IsVisible = true;
+        await CollectionView.FadeTo(1, 1000, Easing.CubicIn);
     }
 
-    private void Button_Clicked_1(object sender, EventArgs e)
+    private async void Button_Clicked_1(object sender, EventArgs e)
     {
+        await CollectionView.FadeTo(0, 1000, Easing.CubicIn);
         CollectionView.IsVisible = false;
     }
 }
