c561623c9710044c67a5b59545ea960c7317db5a
Eloi Jahan <108059693+eloiJhn@users.noreply.github.com>
2023-05-26 20:30:52 +0200

    up

--- ProjetSport/projetsportmaui/index.js
+++ ProjetSport/projetsportmaui/index.js
@@ -7,8 +7,6 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
